5453042db06c6d0c7708ba24ae7acec272fb80ca
Marcin Wolicki <marcin.wolicki@gmail.com>
2017-10-04 00:38:33 +0100

    cleaning up

--- brainfuck.rs
+++ brainfuck.rs
@@ -11,13 +11,13 @@ enum Op {
     DecVal,
     Print,
     Read,
-    While { ops: Vec<Op> }
+    While { ops: Vec<Op> },
 }
 
-#[derive(Debug,Clone)]
+#[derive(Debug, Clone)]
 struct State {
-    curr_ptr :i32,
-    data : HashMap<i32, u8>
+    curr_ptr: i32,
+    data: HashMap<i32, u8>,
 }
 
 extern "C" {
@@ -25,47 +25,49 @@ extern "C" {
     pub fn read_val() -> u8;
 }
 
-fn eval_while(s: State, ops: &Vec<Op>) -> State {
+fn eval_while(s: State, ops: &[Op]) -> State {
     let mut state = s;
     while *(state.data.entry(state.curr_ptr).or_insert(0)) != 0 {
         state = eval_vec(state, ops);
     }
-    return state;
+    state
 }
 
-fn eval_vec(s: State, ops: &Vec<Op>) -> State {
+fn eval_vec(s: State, ops: &[Op]) -> State {
     let mut state = s;
-        for op in ops.iter() {
-            state = eval (state, op);
-        }
-    return state;
+    for op in ops.iter() {
+        state = eval(state, op);
+    }
+    state
 }
 
-fn eval (mut s:State, op:&Op) -> State {
-        {
-            let state = s.clone();
-            let val = s.data.entry(s.curr_ptr).or_insert(0);
-            let wrapped_val = Wrapping(*val);
-            let wrapped_one = Wrapping(1);
-            
-            match op {
-                &Op::IncPointer => s.curr_ptr += 1,
-                &Op::DecPointer => s.curr_ptr -= 1,
-                &Op::IncVal => *val = (wrapped_val + wrapped_one).0,
-                &Op::DecVal => *val = (wrapped_val - wrapped_one).0,
-                &Op::Print => unsafe { print_val(*val) },
-                &Op::Read => *val = unsafe { read_val() },
-                &Op::While { ref ops } => { return eval_while(state, &ops); }
+fn eval(mut s: State, op: &Op) -> State {
+    {
+        let state = s.clone();
+        let val = s.data.entry(s.curr_ptr).or_insert(0);
+        let wrapped_val = Wrapping(*val);
+        let wrapped_one = Wrapping(1);
+
+        match *op {
+            Op::IncPointer => s.curr_ptr += 1,
+            Op::DecPointer => s.curr_ptr -= 1,
+            Op::IncVal => *val = (wrapped_val + wrapped_one).0,
+            Op::DecVal => *val = (wrapped_val - wrapped_one).0,
+            Op::Print => unsafe { print_val(*val) },
+            Op::Read => *val = unsafe { read_val() },
+            Op::While { ref ops } => {
+                return eval_while(state, ops);
             }
         }
-        s
+    }
+    s
 }
 
-fn get_ast (code:&str) -> (Vec<Op>, usize) {
+fn get_ast(code: &str) -> (Vec<Op>, usize) {
     let mut ops = Vec::new();
     let mut i = 0;
     while i < code.len() {
-        let ch = code[(i..i+1)].as_ref();
+        let ch = code[(i..i + 1)].as_ref();
         let op = match ch {
             ">" => Some(Op::IncPointer),
             "<" => Some(Op::DecPointer),
@@ -73,44 +75,46 @@ fn get_ast (code:&str) -> (Vec<Op>, usize) {
             "-" => Some(Op::DecVal),
             "." => Some(Op::Print),
             "," => Some(Op::Read),
-            "[" =>{
-                let (ops, size) = get_ast(code[(i+1..code.len())].as_ref());
-                i+=size+1;
-                match code[(i..i+1)].as_ref() {
-                    "]" => Some(Op::While{ops : ops}),
-                    x => panic!("while loop needs to end with ']' but was with '{:?}'", x)}}
-            "]" => { return (ops, i) } 
+            "[" => {
+                let (ops, size) = get_ast(code[(i + 1..code.len())].as_ref());
+                i += size + 1;
+                match code[(i..i + 1)].as_ref() {
+                    "]" => Some(Op::While { ops: ops }),
+                    x => panic!("while loop needs to end with ']' but was with '{:?}'", x),
+                }
+            }
+            "]" => return (ops, i), 
             _ => None,
         };
         if let Some(op) = op {
-            ops.push (op);
+            ops.push(op);
         }
-        i+=1;        
+        i += 1;
     }
-    return (ops, i)
+    (ops, i)
 }
 
 #[no_mangle]
-pub fn run_code(code:&str) {
-    
-    let init_state = State { curr_ptr : 0, data : HashMap::new() };
+pub fn run_code(code: &str) {
+
+    let init_state = State {
+        curr_ptr: 0,
+        data: HashMap::new(),
+    };
     let (ast, _) = get_ast(&code);
     eval_vec(init_state, &ast);
 }
 
 fn my_string_safe(i: *mut c_char) -> String {
-  unsafe {
-    CStr::from_ptr(i).to_string_lossy().into_owned()
-
-  }
+    unsafe { CStr::from_ptr(i).to_string_lossy().into_owned() }
 }
 
 
 #[no_mangle]
-pub fn js_run_code(code:*mut c_char) {
+pub fn js_run_code(code: *mut c_char) {
     let s = my_string_safe(code);
     println!("code: '{}'", s);
     run_code(&s.as_str());
 }
 
-fn main() { }
+fn main() {}
