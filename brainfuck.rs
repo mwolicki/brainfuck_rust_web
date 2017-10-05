0802fa42b4ac12fe00c02ffb7f16a66c7ce90ee6
Marcin Wolicki <marcin.wolicki@gmail.com>
2017-10-05 03:17:24 +0100

    compact AST

--- brainfuck.rs
+++ brainfuck.rs
@@ -12,11 +12,13 @@ fn read(current_output: &Vec<u8>) -> u8 {
     unsafe { read_val(to_c_str(&current_output)) }
 }
 
+
+#[derive(PartialEq, Clone)]
 enum Op {
-    IncPointer,
-    DecPointer,
-    IncVal,
-    DecVal,
+    IncPointer(usize),
+    DecPointer(usize),
+    IncVal(u8),
+    DecVal(u8),
     Print,
     Read,
     While { ops: Vec<Op> },
@@ -29,6 +31,7 @@ struct State {
     data: [u8; HEAP_SIZE],
     output: Vec<u8>,
 }
+
 
 fn eval_while(state: &mut State, ops: &[Op]) {
     while state.data[state.curr_ptr] != 0 {
@@ -44,22 +47,66 @@ fn eval_vec(state: &mut State, ops: &[Op]) {
 
 fn eval(state: &mut State, op: &Op) {
     match *op {
-        Op::IncPointer => state.curr_ptr = (Wrapping(state.curr_ptr) + Wrapping(1)).0 % HEAP_SIZE,
-        Op::DecPointer => state.curr_ptr = (Wrapping(state.curr_ptr) - Wrapping(1)).0 % HEAP_SIZE,
-        Op::While { ref ops } => {
-            eval_while(state, ops);
+        Op::IncPointer(n) => {
+            state.curr_ptr = (Wrapping(state.curr_ptr) + Wrapping(n)).0 % HEAP_SIZE
         }
-        Op::IncVal => {
-            state.data[state.curr_ptr] = (Wrapping(state.data[state.curr_ptr]) + Wrapping(1)).0
+        Op::DecPointer(n) => {
+            state.curr_ptr = (Wrapping(state.curr_ptr) - Wrapping(n)).0 % HEAP_SIZE
         }
-        Op::DecVal => {
-            state.data[state.curr_ptr] = (Wrapping(state.data[state.curr_ptr]) - Wrapping(1)).0
+        Op::While { ref ops } => eval_while(state, ops),
+        Op::IncVal(n) => {
+            state.data[state.curr_ptr] = (Wrapping(state.data[state.curr_ptr]) + Wrapping(n)).0
         }
+        Op::DecVal(n) => {
+            state.data[state.curr_ptr] = (Wrapping(state.data[state.curr_ptr]) - Wrapping(n)).0
+        }
+
         Op::Print => state.output.push(state.data[state.curr_ptr]),
         Op::Read => state.data[state.curr_ptr] = read(&state.output),
     }
 }
 
+fn compact(ast: &[Op]) -> Vec<Op> {
+    let mut compacted_ast = Vec::new();
+    let mut current_op: Option<Op> = None;
+    let mut count = 0;
+
+    for op in ast {
+        if let Some(curr_op) = current_op.clone() {
+            if *op == curr_op {
+                count += 1;
+            } else {
+                match curr_op {
+                    Op::IncPointer(n) => compacted_ast.push(Op::IncPointer(n + count)),
+                    Op::DecPointer(n) => compacted_ast.push(Op::DecPointer(n + count)),
+                    Op::IncVal(n) => compacted_ast.push(Op::IncVal(n + count as u8)),
+                    Op::DecVal(n) => compacted_ast.push(Op::DecVal(n + count as u8)),
+                    _ => (),
+                }
+                current_op = None;
+                count = 0;
+            }
+        }
+        match *op {
+            Op::While { ref ops } => compacted_ast.push(Op::While { ops: compact(ops) }),
+            Op::Print => compacted_ast.push(Op::Print),
+            Op::Read => compacted_ast.push(Op::Read),
+            _ => current_op = Some(op.clone()),
+        }
+    }
+
+    if let Some(curr_op) = current_op.clone() {
+        match curr_op {
+            Op::IncPointer(n) => compacted_ast.push(Op::IncPointer(n + count)),
+            Op::DecPointer(n) => compacted_ast.push(Op::DecPointer(n + count)),
+            Op::IncVal(n) => compacted_ast.push(Op::IncVal(n + count as u8)),
+            Op::DecVal(n) => compacted_ast.push(Op::DecVal(n + count as u8)),
+            _ => (),
+        }
+    }
+
+    compacted_ast
+}
 
 fn get_ast(code: &[char]) -> (Vec<Op>, usize) {
     let mut ops = Vec::new();
@@ -67,10 +114,10 @@ fn get_ast(code: &[char]) -> (Vec<Op>, usize) {
     while i < code.len() {
         let ch = code[i];
         let op = match ch {
-            '>' => Some(Op::IncPointer),
-            '<' => Some(Op::DecPointer),
-            '+' => Some(Op::IncVal),
-            '-' => Some(Op::DecVal),
+            '>' => Some(Op::IncPointer(1)),
+            '<' => Some(Op::DecPointer(1)),
+            '+' => Some(Op::IncVal(1)),
+            '-' => Some(Op::DecVal(1)),
             '.' => Some(Op::Print),
             ',' => Some(Op::Read),
             '[' => {
@@ -92,7 +139,6 @@ fn get_ast(code: &[char]) -> (Vec<Op>, usize) {
     (ops, i)
 }
 
-
 fn run_brainfuck(code: &str) -> String {
     let mut state = State {
         curr_ptr: 0,
@@ -102,10 +148,10 @@ fn run_brainfuck(code: &str) -> String {
 
     let chars: Vec<char> = code.chars().collect();
     let (ast, _) = get_ast(&chars);
+    let ast = compact(&ast);
     eval_vec(&mut state, &ast);
-    String::from_utf8_lossy(&state.output.as_slice()).into_owned()
+    String::from_utf8_lossy(state.output.as_slice()).into_owned()
 }
-
 
 fn from_c_str(i: *mut c_char) -> String {
     unsafe { CStr::from_ptr(i).to_string_lossy().into_owned() }
--- build_wasm.sh
+++ build_wasm.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 export EMMAKEN_CFLAGS="--js-library external.js" 
+#-s \"BINARYEN_METHOD='native-wasm,asmjs'\"
 rustc --target=wasm32-unknown-emscripten brainfuck.rs -O -o brainfuck.html 
